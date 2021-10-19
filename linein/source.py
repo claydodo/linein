b6d14e500378b61c760cb1735ad9d27f51cf607c
yangdw <claydodo@foxmail.com>
2021-10-19 21:19:27 +0800

    FolderSource add more structures

--- linein/source.py
+++ linein/source.py
@@ -1,4 +1,10 @@
-__all__ = ['Source', 'JSONFileSource', 'JSONFolderSource']
+__all__ = [
+    'Source',
+    'FolderSourceBase',
+    'FileTypeFolderSourceBase',
+    'JSONFolderSource',
+    'JSONFileSource',
+]
 
 import os
 import json
@@ -8,6 +14,40 @@ import glob
 class Source:
     def __iter__(self):
         return
+
+
+class FolderSourceBase(Source):
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+
+    def __iter__(self):
+        for file_path in self.list_files():
+            yield self.load_file(file_path)
+
+    def list_files(self):
+        return glob.iglob(os.path.join(self.folder_path, '*'))
+
+    def load_file(self, file_path):
+        raise NotImplementedError
+
+
+class FileTypeFolderSourceBase(FolderSourceBase):
+    ext = ''
+
+    def list_files(self):
+        if not self.ext:
+            file_pattern = '*'
+        else:
+            file_pattern = '*.{}'.format(self.ext.lstrip('.'))
+        return glob.iglob(os.path.join(self.folder_path, file_pattern))
+
+
+class JSONFolderSource(FileTypeFolderSourceBase):
+    ext = 'json'
+
+    def load_file(self, file_path):
+        with open(file_path) as f:
+            return json.load(f)
 
 
 class JSONFileSource(Source):
@@ -21,12 +61,3 @@ class JSONFileSource(Source):
             return iter(data)
 
 
-class JSONFolderSource(Source):
-    def __init__(self, folder_path):
-        self.folder_path = folder_path
-
-    def __iter__(self):
-        for file_path in glob.iglob(os.path.join(self.folder_path, '**/*.json'), recursive=True):
-            with open(file_path) as f:
-                data = json.load(f)
-                yield data
